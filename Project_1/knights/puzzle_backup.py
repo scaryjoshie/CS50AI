00ae82f2fff78d5f731e833e22051820d49af036
scaryjoshie <67752308+scaryjoshie@users.noreply.github.com>
2023-10-01 12:49:29 -0400

    Finished knights

--- Project_1/knights/puzzle_backup.py
+++ Project_1/knights/puzzle_backup.py
@@ -14,8 +14,14 @@ CKnave = Symbol("C is a Knave")
 knowledge0 = And(
     # A can be a knight or knave but not both
     Or(AKnight, AKnave), Not(And(AKnight, AKnave)),
+
     # A is both a knight and a knave if and only if A is a knight
     Biconditional(AKnight, And(AKnight, AKnave)),
+
+    # If A is a knight, then A is both a knight and a knave
+    #Implication(AKnight, Not(And(AKnight, AKnave)))
+    # If A is a knave, then A is not both a knight and a knave
+    #Implication(AKnave, Not(And(AKnight, AKnave)))
 )
 
 # Puzzle 1
@@ -25,8 +31,16 @@ knowledge1 = And(
     # A and B can each be knights or knaves but not both
     Or(AKnight, AKnave), Not(And(AKnight, AKnave)),
     Or(BKnight, BKnave), Not(And(BKnight, BKnave)),
+
     # A and B are both knaves if and only if A is a knight
     Biconditional(AKnight, And(AKnave, BKnave)),
+    
+    # OLD
+    # If A is a knight, A and B must both be knaves since knights only tell the truth
+    #Implication(AKnight, And(AKnave, BKnave)),
+    # If A is a knave, then A and B are NOT both knaves
+    #Implication(AKnave, Not(And(AKnave, BKnave)))
+    
 )
 
 # Puzzle 2
@@ -36,10 +50,23 @@ knowledge2 = And(
     # A and B can each be knights or knaves but not both
     Or(AKnight, AKnave), Not(And(AKnight, AKnave)),
     Or(BKnight, BKnave), Not(And(BKnight, BKnave)),
+
     # A and B are of the same kind if and only if A is a knight
     Biconditional(AKnight, Or(And(AKnight, BKnight), And(AKnave, BKnave))),
+
     # A and B are of different kinds if and only if B is a knight
     Biconditional(BKnight, Or(And(AKnight, BKnave), And(AKnave, BKnight))),
+
+    # OLD
+    # If A is a knight, both A and B are of the same kind
+    #Implication(AKnight, Or(And(AKnight, BKnight), And(AKnave, BKnave))),
+    # If A is a knave, A and B are not of the same kind
+    #Implication(AKnave, And(Not(And(AKnight, BKnight)), Not(And(AKnave, BKnave)))),
+    # If B is a knight, A and B are of different kinds
+    #Implication(BKnight, Or(And(AKnight, BKnave), And(AKnave, BKnight))),
+    # If B is a knave, A and B are not of different kinds
+    #Implication(BKnave, And(Not(And(AKnight, BKnave)), Not(And(AKnave, BKnight)))),
+
 )
 
 # Puzzle 3
@@ -52,13 +79,18 @@ knowledge3 = And(
     Or(AKnight, AKnave), Not(And(AKnight, AKnave)),
     Or(BKnight, BKnave), Not(And(BKnight, BKnave)),
     Or(CKnight, CKnave), Not(And(CKnight, CKnave)),
+
     # If B is a knight, then A said they are a knave, which means A is a knave if and only if A is a knight
     Implication(BKnight, Biconditional(AKnight, AKnave)),
+
     # C is a knave if and only if B is a knight
     Biconditional(BKnight, CKnave),
+
     # A is a knight if and only if C is a knight
-    Biconditional(CKnight, AKnight),
+    Biconditional(CKnight, AKnight)
+
 )
+
 
 
 def main():
--- Project_1/minesweeper/test.py
+++
@@ -1,3 +0,0 @@
-ex = {1, 2, 3}
-import random
-random.choice(list(ex))
